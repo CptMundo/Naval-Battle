5074f4ea1e82533d0e4163ef924ed8e0081349a7
CptMundo <>
2023-10-13 20:21:52 +0000

    Place boat in a random row and col

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import random
+
 # Create an empty 5x5 grid for the ocean
 ocean = [["O" for _ in range(5)] for _ in range(5)]
 
-for row in ocean:
-    print(' '.join(row))
+# Randomly place a ship
+ship_row = random.randint(1, 5)
+ship_col = random.randint(1, 5)
